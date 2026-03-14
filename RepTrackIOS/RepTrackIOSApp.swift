0c08e0f775cc7b961280e0b3400d30a1489d94f7
Bradacs Laszlo <Bredimindenes@gmail.com>
2026-03-14 18:49:25 +0100

    Initial Commit

---
+++ RepTrackIOS/RepTrackIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepTrackIOSApp.swift
+//  RepTrackIOS
+//
+//  Created by László Bradács on 2026. 03. 14..
+//
+
+import SwiftUI
+
+@main
+struct RepTrackIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
